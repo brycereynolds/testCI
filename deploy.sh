8e22757d1128fe1fda3053bdbaea1b975e92ccfa
Bryce Reynolds <brycereynoldsdesign@gmail.com>
2015-07-10 17:10:46 -0700

    Trying to get ansible-tower-cli to work

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
+tower-cli config host $ANSIBLE_TOWER_SERVER
+tower-cli config username $ANSIBLE_TOWER_USER
+tower-cli config password $ANSIBLE_TOWER_PASS
 
-tower-cli joblaunch \
-    --template $ANSIBLE_TOWER_JOB_TEMPLATE_ID \
-    --username $ANSIBLE_TOWER_USER  \
-    --password $ANSIBLE_TOWER_PASS  \
-    --server $ANSIBLE_TOWER_SERVER
+tower-cli job launch --job-template=$ANSIBLE_TOWER_JOB_TEMPLATE_ID
