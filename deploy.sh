7a9b2ce919f9a6a0c1bc80a93e19a1a2bbf89d2d
Bryce Reynolds <brycereynoldsdesign@gmail.com>
2015-07-13 13:18:42 -0700

    Testing integration with docker and CI

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+# Build image with local dockerfile and push to our docker repo
+VERSION=`grep '"version"' package.json | grep -o "[0-9]\.[0-9]\.[0-9]"`
+
+git tag $VERSION
+git push --tags
+docker build -t="$DOCKER_HUB_REPO:$VERSION" .
+docker push $DOCKER_HUB_REPO:$VERSION
+
+# Tell ansible to fire off a job
 tower-cli config host $ANSIBLE_TOWER_SERVER
 tower-cli config username $ANSIBLE_TOWER_USER
 tower-cli config password $ANSIBLE_TOWER_PASS
